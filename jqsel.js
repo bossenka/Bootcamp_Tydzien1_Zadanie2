76d65b5b9e45315defac1a3950636a8b4f2d2db8
bossenka <fatyga.m.90@gmail.com>
2016-11-11 13:21:55 +0100

    Add solution.

---
+++ jqsel.js
@@ -0,0 +1,10 @@
+// 1.
+$('div.grid.grid-12');
+// 2.
+$('.nav a[href^="http"]');
+// 3.
+$('input:radio:not(:checked), input:checkbox:not(:checked)');
+// 4.
+$('div#text p:empty:first');
+// 5.
+$('.pagination-item:not(span)');
